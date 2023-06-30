bbeb8041255e61524ea0568dcd1c3284aececc01
sechi <sechichen@gmail.com>
2023-06-30 15:05:47 +0800

    chore: support jsx/tsx

--- projects/vue-components/vite.config.ts
+++ projects/vue-components/vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
